bb4a6c73fa81db7de8105f4d7a3679eb46abce05
NayrL <40843838+NayrL114@users.noreply.github.com>
2022-10-05 21:39:01 +1100

    50 percent complete

---
+++ Assets/Scripts/MoveSphere.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveSphere : MonoBehaviour
+{
+
+    private Vector3 target = new Vector3(-3f, 1f, 0f);
+    private float duration = 1.5f;
+    public Tweener twn;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (twn.TweenExists(transform))
+        if (!twn.TweenExists(transform))
+        {
+            target.x = -target.x;
+            target.y = -target.y;
+            twn.AddTween(transform, transform.position, target, duration/SpeedManager.SpeedModifier);
+        }
+    }
+}
